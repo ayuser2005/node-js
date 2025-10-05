ec0f97e19c603a8dbecc0cfdd63ca991ac1810c6
ayuser2005 <anand23154040@akgec.ac.in>
2025-10-05 09:07:53 +0000

    learning fs module

--- .devcontainer/file.js
+++ .devcontainer/file.js
@@ -4,7 +4,16 @@ const fs=require("fs");
 //fs.writeFileSync("./test.txt","Hey there");
 
 //Async
-fs.writeFile("./test.txt","Hello World Async", (err)=>{});
+//fs.writeFile("./test.txt","Hello World Async", (err)=>{});
 
 //const result=fs.readFileSync("./contact.txt","utf-8");
 //console.log(result);
+
+fs.appendFileSync("./test.txt",`${Date.now()}Hey There\n`);
+
+//fs.cpSync("./test.txt","./copy.txt");
+//fs.unlinkSync("./copy.txt");
+
+console.log(fs.statSync("./test.txt"));
+
+fs.mkdirSync("my-docss/a/b",{recursive:true});
