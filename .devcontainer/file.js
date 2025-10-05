33494beff07ad5e5a1ae4f6992f1aeee7c88549d
ayuser2005 <anand23154040@akgec.ac.in>
2025-10-05 08:44:52 +0000

    container config

--- .devcontainer/file.js
+++ .devcontainer/file.js
@@ -1,4 +1,10 @@
 const fs=require("fs");
 
 //Sync.. call
-fs.writeFileSync("./test.txt","Hey there");
+//fs.writeFileSync("./test.txt","Hey there");
+
+//Async
+fs.writeFile("./test.txt","Hello World Async", (err)=>{});
+
+//const result=fs.readFileSync("./contact.txt","utf-8");
+//console.log(result);
